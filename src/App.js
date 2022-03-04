a3c60b448db4ac4d183a7428a45fdcd1bfe6c4c0
Anna Dragan <anulkadragan@gmail.com>
2022-03-04 12:00:09 +0100

    Add delete function

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import { localItems } from './db';
 import ListItem from './components/ListItem';
 import './styles.css';
 import Header from './components/Header';
-import AddItem from './components/AddItem';
+// import AddItem from './components/AddItem';
 import Subheader from './components/Subheader';
+import { useState } from 'react';
+// import { nanoid } from 'nanoid';
 
 function App() {
+  const [articles, setArticles] = useState(localItems);
+
+  function handleOnDelete(articleId) {
+    setArticles(articles.filter(article => article._id !== articleId));
+  }
   return (
     <>
       <Header text="Shopping List" />
-      <ListItem initialItems={localItems} />
+      <ListItem curry={articles} onDelete={handleOnDelete} />
       <Subheader question="What do you want to buy?" />
-      <AddItem />
+      {/* <AddItem />*/}
     </>
   );
 }
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,12 +1,15 @@
 import './ListItem.css';
 
-export default function ListItem({ initialItems }) {
+export default function ListItem({ curry, onDelete }) {
   return (
-    <ul className="Shopping__list" role="list">
-      {initialItems.map(item => {
+    <ul className="Shopping__list">
+      {curry.map(item => {
         return (
-          <li className="Shopping__list__item" key={initialItems._id}>
-            <button className="Shopping__list__item__button">
+          <li className="Shopping__list__item" key={curry._id}>
+            <button
+              className="Shopping__list__item__button"
+              onClick={() => onDelete(item._id)}
+            >
               {item.name.en}
             </button>
           </li>
