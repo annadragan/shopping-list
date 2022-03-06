7856126e3c97e7fa9940189212a2a96f611f8749
Anna Dragan <anulkadragan@gmail.com>
2022-03-06 17:14:46 +0100

    Hide api-list

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
           setSearchValue(event.target.value);
         }}
       ></input>
-      <Fetch value={searchValue} />
+      {searchValue && <Fetch value={searchValue} />}
     </>
   );
 }
